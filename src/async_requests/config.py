399b48740d8ff46d25f620f6eb8e8c3ea99fe2c7
Volker Lorrmann <volker.lorrmann@gmail.com>
2022-09-04 22:37:45 +0200

    Added load_dotenv to config

--- src/async_requests/config.py
+++ src/async_requests/config.py
@@ -1,11 +1,14 @@
+from dotenv import load_dotenv
+import os
 from .utils import get_user_agents, get_webshare_proxy_list, get_free_proxy_list
-
 
 
 USER_AGENTS = get_user_agents()
 
+# Set the variable URL_WEBSHARE_PROXY_LIST in your .env
+load_dotenv()
+URL_WEBSHARE_PROXY_LIST = os.getenv("URL_WEBSHARE_PROXY_LIST", None)
 
-URL_WEBSHARE_PROXY_LIST = None # Put your webshare.io proxy list download url here
 
 if URL_WEBSHARE_PROXY_LIST is not None:
     PROXIES = get_webshare_proxy_list(URL_WEBSHARE_PROXY_LIST)
